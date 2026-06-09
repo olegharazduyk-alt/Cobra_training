90946def991217e0f2732bec5b6b432a5b050902
olegharazduyk-alt <oleg.harazduyk@gmail.com>
2026-06-09 12:07:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -227,5 +227,5 @@ async def next_ex(message: types.Message, state: FSMContext):
 async def main():
     await dp.start_polling(bot)
 
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
